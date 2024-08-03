7b2e0e3ca8dd604fd29b5994bd60be48e33d6e46
Nelly Simkova <sk8high3r@ya.ru>
2024-08-03 20:48:15 +0300

    Generate Column model

---
+++ db/migrate/20240803174742_create_columns.rb
@@ -0,0 +1,10 @@
+class CreateColumns < ActiveRecord::Migration[7.1]
+  def change
+    create_table :columns do |t|
+      t.string :name
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
