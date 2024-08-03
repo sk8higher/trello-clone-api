cca47fbc1e622637443f01179eb4eb3eea1f078b
Nelly Simkova <sk8high3r@ya.ru>
2024-08-03 23:10:02 +0300

    Add Card model, associations and validations

--- app/models/column.rb
+++ app/models/column.rb
@@ -1,5 +1,6 @@
 class Column < ApplicationRecord
   belongs_to :user
+  has_many :cards
 
   validates :name, presence: true, length: { minimum: 5, maximum: 50 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 8 }, presence: true
 
   has_many :columns
+  has_many :cards, through: :columns
 end
